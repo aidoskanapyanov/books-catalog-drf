9d3089b150c33a4e1353a4114473a0c14a9ef1eb
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-11-01 18:00:49 +0600

    Add filtering

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+
 from app.models import Book
 from app.serializers import BookSerializer
 
@@ -6,3 +8,9 @@ from app.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'publication_date': ['gte', 'lte'],
+        'genres__name': ['icontains'],
+        'authors__name': ['icontains'],
+    }
