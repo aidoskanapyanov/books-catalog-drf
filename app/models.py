4539e901f73a0f794ed6a4f61d306ac9784e6189
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-11-01 12:23:19 +0600

    Add models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    publication_date = models.DateField()
+    genres = models.ManyToManyField(Genre)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    text = models.TextField()
+
+
+class FavoriteBook(models.Model):
+    user = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+
+# Additional user profile model to store user favorites
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_books = models.ManyToManyField(Book, through='FavoriteBook')
+
+    def __str__(self):
+        return self.user.username
--- project/settings.py
+++ project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'django_extensions',
     "corsheaders",
+    "app",
 ]
 
 MIDDLEWARE = [
